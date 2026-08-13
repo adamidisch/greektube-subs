bc5e1aca1ae5e9a5a3400f22850b0ddeb2d9087f
adamidisch <136883594+adamidisch@users.noreply.github.com>
2026-08-13 16:00:38 +0300

    chore: wire cue editor preview

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,11 @@
 import GreekTubePlayer from "./GreekTubePlayer";
+import CueEditEnhancer from "./CueEditEnhancer";
 
 export default function Home() {
-  return <GreekTubePlayer />;
+  return (
+    <>
+      <GreekTubePlayer />
+      <CueEditEnhancer />
+    </>
+  );
 }
