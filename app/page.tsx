c1c9d22960178ca0ab51377c8b1245ec430447e0
adamidisch <136883594+adamidisch@users.noreply.github.com>
2026-08-13 18:29:09 +0300

    Enable player UX enhancer

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import GreekTubePlayer from "./GreekTubePlayer";
 import CueEditEnhancer from "./CueEditEnhancer";
+import PlayerUXEnhancer from "./PlayerUXEnhancer";
 
 export default function Home() {
   return (
     <>
       <GreekTubePlayer />
       <CueEditEnhancer />
+      <PlayerUXEnhancer />
     </>
   );
 }
