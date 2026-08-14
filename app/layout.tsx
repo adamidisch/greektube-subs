fe64379b34706d025241da47ed2e74d37d956f3e
adamidisch <136883594+adamidisch@users.noreply.github.com>
2026-08-14 14:25:26 +0300

    Load v7.8.9 design overrides

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,6 +18,7 @@ import "./v7-5-3.css";
 import "./v7-6-0.css";
 import "./v7-6-1.css";
 import "./claude-ui-v78.css";
+import "./v7-8-9-design.css";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
