1e5de8fdb7d9b36d912285f827930f3cdcbb6fdf
adamidisch <136883594+adamidisch@users.noreply.github.com>
2026-08-15 08:58:43 +0300

    style: mount reviewed Claude logo and modal layers

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,6 +26,8 @@ import "./v7-8-11-light-polish.css";
 import "./v7-8-12-theme-toggle.css";
 import "./v7-8-13-designerui.css";
 import "./v7-8-13-reference-controls-footer.css";
+import "./v7-8-14-brand-mark-svg.css";
+import "./v7-8-15-modal-shell.css";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
