a3e64ea0f36ba0084c43b8f26b7d264b344a477d
adamidisch <136883594+adamidisch@users.noreply.github.com>
2026-08-16 16:12:18 +0300

    ui: load settings screen isolation last

--- app/layout.tsx
+++ app/layout.tsx
@@ -30,6 +30,7 @@ import "./v7-8-15-modal-shell.css";
 import "./v7-8-16-home-title-polish.css";
 import "./v7-8-17-mobile-viewer-polish.css";
 import "./brand.css";
+import "./screen-isolation.css";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
